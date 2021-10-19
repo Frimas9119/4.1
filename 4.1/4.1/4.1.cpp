dd3833ff06884d6add01392be022caea396e24a3
Frimas9119 <90787948+Frimas9119@users.noreply.github.com>
2021-10-19 12:01:31 +0300

    Add files via upload

---
+++ 4.1/4.1/4.1.cpp
@@ -0,0 +1,43 @@
+﻿// Lab_04_1.cpp
+// < Тимчук Остап >
+// Лабораторна робота No 4.1
+// Цикли.
+// Варіант 8
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int k, N, i;
+	double p;
+	cout << "k = "; cin >> k;
+	cout << "N = "; cin >> N;
+	p = 0;
+	i = k;
+	while (i <= N)
+	{
+		p+=(k/i)+(i/N);
+		i++;
+	}
+	cout << p << endl;
+		p = 0;
+	i = k;
+	do {
+		p += (k / i) + (i / N);
+		i++;
+	} while (i <= N);
+	cout << p << endl;
+	p = 0;
+	for (i = k; i <= N; i++)
+	{
+		p += (k / i) + (i / N);
+	}
+	cout << p << endl;
+	p = 0;
+	for (i = N; i >= k; i--)
+	{
+		p += (k / i) + (i / N);
+	}
+	cout << p << endl;
+	return 0;
+}
